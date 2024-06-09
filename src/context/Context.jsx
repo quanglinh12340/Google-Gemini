f2ecfbab474960fe9e2edeab5c8bcf3bef864905
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-09 22:59:58 +0700

    create file Context to use for other file

---
+++ src/context/Context.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState } from "react";
+import run from "@/config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompt, setPrevPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    const response = await run(input);
+    setResultData(response);
+    setLoading(false);
+    setInput("");
+  };
+
+  const contextvalue = {
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    prevPrompt,
+    setPrevPrompt,
+    showResult,
+    loading,
+    resultData,
+    onSent,
+  };
+
+  return (
+    <Context.Provider value={contextvalue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
